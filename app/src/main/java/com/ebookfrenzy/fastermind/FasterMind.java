595252121a69ad285399810d5da440eadce5e90a
Jacob Hedman <Jacob.Hedman1@gmail.com>
2016-10-18 21:16:27 -0400

    added the rest of the project

---
+++ app/src/main/java/com/ebookfrenzy/fastermind/FasterMind.java
@@ -0,0 +1,13 @@
+package com.ebookfrenzy.fastermind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FasterMind extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faster_mind);
+    }
+}
