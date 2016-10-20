2ba0b8f264e95ee8dc885c635ab3e48028759fcc
Jacob Hedman <Jacob.Hedman1@gmail.com>
2016-10-20 17:59:49 -0400

    Initalized the buttons and editable texts within FasterMind.java

--- app/src/main/java/com/ebookfrenzy/fastermind/FasterMind.java
+++ app/src/main/java/com/ebookfrenzy/fastermind/FasterMind.java
@@ -2,13 +2,36 @@ package com.ebookfrenzy.fastermind;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FasterMind extends AppCompatActivity {
+    //Fields
+        private final int WORD_LENGTH = 4;
 
+        private TextView targetWord;
+        private TextView computerWord;
+        private TextView numOfGuesses;
+        private TextView letterBeingChecked;
+
+        private Button rightLrightPosBtn;
+        private Button rightLwrongPosBtn;
+        private Button wrongLwrongPosBtn;
+        private Button resetBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faster_mind);
+
+        targetWord = (TextView) findViewById(R.id.targetWord);
+        computerWord = (TextView) findViewById(R.id.computerWord);
+        numOfGuesses = (TextView) findViewById(R.id.numOfGuesses);
+        letterBeingChecked = (TextView) findViewById(R.id.letterBeingChecked);
+
+        rightLrightPosBtn = (Button) findViewById(R.id.rightLrightPosition);
+        rightLwrongPosBtn = (Button) findViewById(R.id.rightLwrongPosition);
+        wrongLwrongPosBtn = (Button) findViewById(R.id.wrongLwrongPosition);
+        resetBtn = (Button) findViewById(R.id.reset);
     }
 
 
